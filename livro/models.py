a4285b1aa1870157d798ab7dca9768ee755eb9fe
Rian dos Passos <riandpf2002@gmail.com>
2022-04-03 17:38:42 -0300

    Configurações iniciais

---
+++ livro/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Livros(models.Model):
+    nome = models.CharField(max_length = 100)
+    autor = models.CharField(max_length = 30)
+    co_autor = models.CharField(max_length = 30)
+    data_cadastro = models.DateField()
+    emprestado = models.BooleanField(default = False)
+    nome_emprestado = models.CharField(max_length = 30)
+    data_emprestimo = models.DateTimeField()
+    data_devolucao = models.DateTimeField()
+    tempo_duracao = models.DateField()
